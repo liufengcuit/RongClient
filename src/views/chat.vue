e8a42e4b2cc77ca5e115a2c41d19f430b37ce770
Liufeng <1048769816@qq.com>
2019-05-20 17:52:30 +0800

    first commit

---
+++ src/views/chat.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="chat">dsaf</div>
+</template>
+<script>
+    import { initSdk } from '../Rong/init.js';
+    export default {
+        data() {
+            return {}
+        },
+        created() {
+            this.login();
+        },
+        methods: {
+            login() {
+                this.axios({
+                    methods: 'get',
+                    url: 'http://127.0.0.1:8090/login',
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+                    }
+                }).then(res=>{
+                    console.log(res.data)
+                    initSdk("cpj2xarlctw0n", res.data.token)
+                })
+                .catch(err=>{
+                    console.log(err)
+                })
+            }
+        }
+    }
+</script>
